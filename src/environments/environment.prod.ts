f79918da38a0dfc6bd8bac46eb4249c39cfbd187
ajinkyakadhe <ajinkyakadhe786@gmail.com>
2022-04-04 19:51:35 +0530

    base url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   BASE_URL:"https://adminapi.famelinks.in/v1/",
   // MUSIC_BASE_URL:"https://famelinks.s3.ap-south-1.amazonaws.com/funlinks-songs/",
-  MUSIC_BASE_URL:"https://famelinks-dev.s3.ap-south-1.amazonaws.com/funlinks-songs/"
+  MUSIC_BASE_URL:"https://famelinks.s3.ap-south-1.amazonaws.com/funlinks-songs/"
 
 };
